8b097b52c37027b00cf0cac465b17d935c1fb78b
Paola Navas <dpaonc@outlook.es>
2020-11-25 22:42:59 +0100

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+   var numIt =10;
+    repeat(numIt){
+
+        repeat(numIt-it){
+            print(" ");
+        }
+        print(" /");
+
+        repeat(it*2){
+            print("_");
+        }
+        println("\\");
+    }
+}
